d236808e8fddd8328b95f93c0690dd65984ce316
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-15 02:20:26 +0300

    Fixed Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footter = () => {
     return (
         <footer className="info">
-            <p>Manage Employee App</p>
+            <p>Book App For Learn Context Api</p>
             <p>Created by <a href="https://www.linkedin.com/in/alicolak64/">Ali Colak</a></p>
             <p>View Code on  <a href="https://github.com/alicolak64/book-project-2">Github</a></p>
         </footer>
