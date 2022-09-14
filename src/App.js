e1096d03e0e1ad900b8cba04298c18607667d160
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-15 02:14:27 +0300

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import BookList from './/components/BookList';
+import Footer from './components/Footer/Footer';
 
 import BookContextProvider from './/contexts/BookContext';
 import ThemeContextProvider from './/contexts/ThemeContext';
+
+
 
 function App() {
 
   return (
 
-    <ThemeContextProvider>
-      <BookContextProvider>
-        <BookList />
-      </BookContextProvider>
-    </ThemeContextProvider>
+    <>
+      <ThemeContextProvider>
+        <BookContextProvider>
+          <BookList />
+        </BookContextProvider>
+      </ThemeContextProvider>
 
+      <Footer />
+    </>
   );
 }
 
